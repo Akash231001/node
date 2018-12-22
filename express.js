0161fc9276aa515b200b35fa145b737fe36e2c44
Akash231001 <40402319+Akash231001@users.noreply.github.com>
2018-12-22 16:43:05 +0530

    Update express.js
    just removed all the commenting

--- express.js
+++ express.js
@@ -45,7 +45,69 @@ for (var i = 0; i <= 100; i++) {
 // console.log('`$v`')
 // let str = "John Smith";
 
-// console.log(str.replace(/(John) (Smith)/, '$2, $1')) // Smith
+// console.log(str.replace(/(John) (Smith)/, '$var arr=[1,2,3,45,8,7,9]
+// console.log(arr.sort())
+function passInFunction(a,b) {
+	// body...
+	return a-b
+}
+console.log(arr.sort(passInFunction))
+
+
+var arr=['a','v','g','t','Y','S','d']
+// console.log(arr.sort())
+function sortingMagic(a,b) {
+	// body...
+	return a-b
+}
+// console.log(arr.sort(sortingMagic))
+arr.sort(function (a,b) {
+	// body...
+	let A=a.toLowerCase()
+	let B=b.toLowerCase()
+	if(A<B)
+		return -1
+	else
+		return 1
+	return 0
+})
+console.log(arr)
+
+var a = 2||3
+let b = 2&&3
+console.log(a)
+console.log(b)
+for (var i = 0; i <= 100; i++) {
+	if(i)
+}
+var vh={}
+var arr=[1,5,8,6,1,2,5]
+arr.forEach(function(val) {
+	vh[val]=(vh[val]||0)+1
+})
+console.log(vh)
+var str='Ak123ash123456gupta 123 132'
+console.log(str.match(/123/ig))
+let v='demo'
+console.log('`$v`')
+let str = "John Smith";
+
+console.log(str.replace(/(John) (Smith)/, '$2, $1')) // Smith
+
+let str = "A lot about JavaScript at https://javascript.info";
+
+let regexp = /JAVA(SCRIPT)/ig;
+
+// Look for the first match
+let matchOne = regexp.exec(str);
+alert( matchOne[0] ); // JavaScript
+alert( matchOne[1] ); // script
+alert( matchOne.index ); // 12 (the position of the match)
+alert( matchOne.input ); // the same as str
+
+alert( regexp.lastIndex ); // 22 (the position after the match)
+
+`console.log${akash}`2, $1')) // Smith
 
 // let str = "A lot about JavaScript at https://javascript.info";
 
